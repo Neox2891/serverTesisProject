9099660d6880de4a72d3dd57195386c8024e1a6c
neox2891 <n.estrada2891@gmail.com>
2018-10-22 23:36:32 -0500

    +react framework integrations

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: 'babel-loader',
+        }]
+    }
+}
